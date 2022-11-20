6ca3c16d2b9a26c6a6703ed9f260e8df23c6978a
Dasha-coder <58812818+Dasha-coder@users.noreply.github.com>
2022-11-20 21:45:39 +0200

    first exersice

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let idInt;
+
+function onStartClick(event) {
+  idInt = setInterval(() => {
+    const colorCurrent = getRandomHexColor();
+    refs.body.style.backgroundColor = `${colorCurrent}`;
+  }, 1000);
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  console.dir(event.currentTarget);
+}
+
+//idInt = setInterval(onStartClick, 1000);
+
+function onStopClick() {
+  clearInterval(idInt);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
